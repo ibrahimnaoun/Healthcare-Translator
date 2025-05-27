685283d5c3dabb7a9c41ab2adb0c2b6db50237e4
Ibrahim Naoun <ibrahimnaoun@gmail.com>
2025-05-27 10:21:47 +0100

    Fix tsconfig and declare SpeechRecognition globals

---
+++ src/globals.d.ts
@@ -0,0 +1,8 @@
+// Declare the SpeechRecognition API for TypeScript
+interface Window {
+  webkitSpeechRecognition: any;
+}
+
+declare var webkitSpeechRecognition: any;
+declare var SpeechRecognition: any;
+declare var SpeechRecognitionEvent: any;
